7943b0e7f57008b6388a1388f988004437391a45
Danielle Collins <dmcollin@unca.edu>
2012-10-03 15:18:32 -0400

    adds usage of an array of regular expressions for testing

--- jasmine-mods.js
+++ jasmine-mods.js
@@ -1,23 +1,29 @@
 jasmine.stackTraceFilter = function(msg) {
-
-    /**
-    todo: rewrite utilizing a defined array of regular expressions (something like 
-      var regexList = ['/jasmine:\d+/', 'jermaine:\d+'];)
-    */
-
+    var regexList = [/jasmine.js/, /jermaine.js/]; //array of regular expressions to test for
+    
     var splitMsg = msg.split("\n"); //create an array of messages separated by \n
     var finalMsg = splitMsg[0] + "\n"; //start the final message with the first line containing the type error
 
+    function regexTest(msgString){
+      for (j=0;j<regexList.length;j++){ //loop through the regexList array
+
+        if (msgString.match(regexList[j])){ //test for match
+          return true; //if match, return true
+        }
+      } 
+    }
+
     for (i=1; i<splitMsg.length; i++){  //loop through the message array
- 
-      if (splitMsg[i].indexOf("jasmine.js") == -1){ //test for messages containing jasmine.js
-        finalMsg += splitMsg[i] + "\n";  //if the message does not contain jasmine.js, add to the final message for output
+      var currMsg = splitMsg[i];
+      if (!regexTest(currMsg)){  //send msg string to regexTest function
+        finalMsg += splitMsg[i] + "\n";  //if msg doesn't match any of the regexList array items, add to the final message
       }
     }
 
     return finalMsg;
 
 };
+
 
 jasmine.HtmlReporter.SpecView.prototype.appendFailureDetail = function() {
   this.detail.className += ' ' + this.status();
